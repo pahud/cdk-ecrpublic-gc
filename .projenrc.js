9c24d8257287d1fa08b3120d226816433d97740e
Pahud Hsieh <pahudnet@gmail.com>
2021-07-24 12:56:31 +0800

    bump versions (#62)

--- .projenrc.js
+++ .projenrc.js
@@ -26,6 +26,7 @@ const project = new AwsCdkConstructLibrary({
     'esbuild',
   ],
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
       secret: AUTOMATION_TOKEN,
